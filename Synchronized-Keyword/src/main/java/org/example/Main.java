a974e90a7cfa3c650ea5789d33a86709aa3fb6e6
CharakaMihiranga <charakavmihiranga@gmail.com>
2024-04-21 11:20:52 +0530

    Synchronized-Keyword

---
+++ Synchronized-Keyword/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+class Calculation{
+
+    int num;
+
+    public void increment(){
+        num++;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Calculation calculation = new Calculation();
+//
+//        for (int i = 0; i < 1000; i++) {
+//
+//            calculation.increment();
+//
+//        }
+
+        Thread t1 = new Thread( () -> {
+
+        });
+    }
+}
