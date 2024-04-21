c1a4015d11802013dda9ae00bbeca88d7e5f718d
CharakaMihiranga <charakavmihiranga@gmail.com>
2024-04-21 11:31:51 +0530

    Synchronized-Keyword

--- Synchronized-Keyword/src/main/java/org/example/Main.java
+++ Synchronized-Keyword/src/main/java/org/example/Main.java
@@ -10,9 +10,9 @@ class Calculation{
 }
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-//        Calculation calculation = new Calculation();
+        Calculation calculation = new Calculation();
 //
 //        for (int i = 0; i < 1000; i++) {
 //
@@ -20,8 +20,24 @@ public class Main {
 //
 //        }
 
-        Thread t1 = new Thread( () -> {
-
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                calculation.increment();
+            }
         });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                calculation.increment();
+            }
+        });
+
+        t1.start();
+        t1.join();
+
+        t2.start();
+        t2.join();
+
+        System.out.println(calculation.num);
     }
 }
